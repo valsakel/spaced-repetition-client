b58a086b03c1b4042455bf894447e3c5a1f21e55
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-07-09 14:39:25 -0500

    Fix config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,3 @@
 export const API_BASE_URL =
     process.env.REACT_APP_API_BASE_URL || 'https://spaced-rep-marjef-server.herokuapp.com/api';
 // process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
-
-
-export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000'
