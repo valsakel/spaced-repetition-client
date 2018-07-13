3bdd5d76d1cd26f3ccac2b8f36455a6dd2688382
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-07-13 00:37:48 -0500

    refactor landing page

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -2,21 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 
-import LoginForm from './login-form';
-
 import './landing-page.css';
 import algorithm_icon from '../images/algorithm.svg'
 import pantone_icon from '../images/pantone.svg'
 import alarmclock_icon from '../images/alarm-clock.svg';
 
-
-
 export function LandingPage(props) {
-  console.log('LANDING PAGE', props);
   // // If we are logged in redirect straight to the user's dashboard
-  // if (props.loggedIn) {
-  //     return <Redirect to="/dashboard" />;
-  // }
+  if (props.loggedIn) {
+    return <Redirect to="/dashboard" />;
+  }
 
   return (
     <React.Fragment>
@@ -32,31 +27,21 @@ export function LandingPage(props) {
           >
             Check it out
           </Link>
-          {/*<a*/}
-          {/*href="#landing-page-how-works-wrapper"*/}
-          {/*className="landing-page-how-works-link"*/}
-          {/*aria-label="Click to show how it works section"*/}
-          {/*>*/}
-          {/*How it works*/}
-          {/*</a>*/}
-          {/*</div>*/}
         </section>
         <section className="lp-about-section">
           <h3>What makes FlashFluent unique?</h3>
           <div className="lp-about-section-details">
             <div>
-              {/*<div>*/}
               <img
                 className="lp-about-section-icon"
                 src={alarmclock_icon}
                 alt="algorithm"
               />
-              {/*</div>*/}
 
               <p>
-                Built-in timer will remind you to take breaks every 20 minutes
-                to re-energize.
-
+                Questions are scored and shown less and less the more you get it right. Wrong answers will be reinforced by being shown again sooner than later.
+                {/* Built-in timer will remind you to take breaks every 20 minutes
+                to re-energize. */}
               </p>
             </div>
             <div>
@@ -77,20 +62,11 @@ export function LandingPage(props) {
               />
               <p>
                 Learn new words faster through a built-in spaced repetition technique.
-
               </p>
             </div>
           </div>
-
-
         </section>
       </main>
-      {/*<LoginForm />*/}
-
-      {/*<Link to="/register">Check it out</Link>*/}
-      {/*<div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>*/}
-      {/*<div>Icons made by <a href="https://www.flaticon.com/authors/alfredo-hernandez" title="Alfredo Hernandez">Alfredo Hernandez</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>*/}
-      {/*<div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>*/}
     </React.Fragment>
 
   );
